bdfd7b1c4364cb931a0b4028135bb09f08a777d2
Artur Faruga <farugaa.training@gmail.com>
2017-11-13 15:51:51 +0100

    first commit

---
+++ src/main/java/com/akademiakodu/thymeleaf/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.akademiakodu.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String index(ModelMap  modelMap) {
+        modelMap.addAttribute("message", "hello akademia");
+
+        return "index";  //Thymeleaf //file path
+    }
+
+    @GetMapping("/{tekst}")
+    public String indexWithText(ModelMap  modelMap, @PathVariable String tekst) {
+        modelMap.addAttribute("message", tekst);
+
+        return "hello/hello";
+    }
+}
